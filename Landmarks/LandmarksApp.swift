61963d5e26e64d34d0963e71d57a26a7030b3bf4
ParnaMaliot <ParnaMaliot@users.noreply.github.com>
2021-04-01 19:54:06 +0200

    Upload

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Igor Parnadjiev on 1.4.21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
